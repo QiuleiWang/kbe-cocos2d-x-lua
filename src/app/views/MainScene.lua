4df41645c236f6dfef6f2b38777792f87cecae1a
qiulei <315884562@qq.com>
2017-10-17 10:03:26 +0800

    no message

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -1,9 +1,20 @@
 
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 function MainScene:onCreate()
-		KBEngine.Event.fire("createAccount","kbe2017","kbe", "kbengine_cocos2d_js_demo");   	
+		--KBEngine.Event.fire("createAccount","kbe2017","kbe", "kbengine_cocos2d_js_demo");   	
     	KBEngine.Event.fire("login","kbe","kbe", "kbengine_cocos2d_js_demo");
     	--KBEngine.Event.register("onReqAvatarList", self, "onReqAvatarList");
+    	KBEngine.Event.register("onLoginSuccessfully", self, "onLoginSuccessfully");
+
+end
+
+function MainScene:onLoginSuccessfully()
+		print("loginn success")
+		local player=KBEngine.app:player()
+		dump(player)
+		if player==nil then
+			print("用户为nil")
+		end
 end
 
 function MainScene:onReqAvatarList(avatars)
