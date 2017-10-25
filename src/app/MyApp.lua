1318c2d321bc13e506ef1f4e262770fa652fc16d
qiulei <315884562@qq.com>
2017-10-25 21:43:01 +0800

    no message

--- src/app/MyApp.lua
+++ src/app/MyApp.lua
@@ -12,7 +12,7 @@ cc.exports["UITableView"]= require("views.ui.UITableView")
 function MyApp:onCreate()
     math.randomseed(os.time())
     local args=KBEngine.KBEngineArgs()
-    args.ip = "103.72.166.71"
+    args.ip = "127.0.0.1"
 	args.port = 20013
     KBEngine.new(args)
 end
